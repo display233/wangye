65704129fbec0951857a9e375e7febdee0b12d38
display233 <114346532+display233@users.noreply.github.com>
2025-02-25 03:42:52 +0800

    Update upload_file.php

--- upload_file.php
+++ upload_file.php
@@ -26,15 +26,15 @@ if ((($_FILES["file"]["type"] == "image/gif")
 		
 		// 判断当期目录下的 upload 目录是否存在该文件
 		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-		if (file_exists("upload/" . $_FILES["file"]["name"]))
+		if (file_exists("/" . $_FILES["file"]["name"]))
 		{
 			echo $_FILES["file"]["name"] . " 文件已经存在。 ";
 		}
 		else
 		{
 			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-			move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
-			echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+			move_file($_FILES["file"]["tmp_name"], "/" . $_FILES["file"]["name"]);
+			echo "文件存储在: " . "/" . $_FILES["file"]["name"];
 		}
 	}
 }
