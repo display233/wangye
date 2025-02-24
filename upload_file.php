d111582a2e420f16181467ac30dd5b45e6aa7c58
display233 <114346532+display233@users.noreply.github.com>
2025-02-25 03:55:37 +0800

    Add files via upload

--- upload_file.php
+++ upload_file.php
@@ -23,18 +23,7 @@ if ((($_FILES["file"]["type"] == "image/gif")
 		echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
 		echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
 		echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
-		
-		// 判断当期目录下的 upload 目录是否存在该文件
-		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-		if (file_exists("/" . $_FILES["file"]["name"]))
-		{
-			echo $_FILES["file"]["name"] . " 文件已经存在。 ";
-		}
-		else
-		{
-			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-			move_upload_file($_FILES["file"]["tmp_name"], "/" . $_FILES["file"]["name"]);
-			echo "文件存储在: " . "/" . $_FILES["file"]["name"];
+		//	move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
 		}
 	}
 }
