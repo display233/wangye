cebdd6df7ce9219ba0a6ba7515da811a18100e44
display233 <114346532+display233@users.noreply.github.com>
2025-02-27 18:34:19 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+
+<body>
+    <?php
 // 创建一个新cURL资源>
 $ch = curl_init();
 
@@ -6,4 +16,8 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://index.html");
 curl_setopt($ch, CURLOPT_AUTOREFERER, "https://weibo.com/");
 header('Location: http://index.html');
+echo '哈哈'
 ?>
+</body>
+
+</html>
