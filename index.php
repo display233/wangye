e5eabe1c015a59e3e657257e73e6160b64c047c6
display233 <114346532+display233@users.noreply.github.com>
2025-02-27 18:38:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 
 <body>
     <?php
-// 创建一个新cURL资源>
+/*// 创建一个新cURL资源>
 $ch = curl_init();
 
 // 设置URL和相应的选项
 curl_setopt($ch, CURLOPT_URL, "http://index.html");
 curl_setopt($ch, CURLOPT_AUTOREFERER, "https://weibo.com/");
-header('Location: http://index.html');
+header('Location: http://index.html');*/
 echo '哈哈'
 ?>
 </body>
